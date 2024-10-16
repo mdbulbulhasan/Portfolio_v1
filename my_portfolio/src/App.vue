6236c18ee7fb18a062e55ff3050d6d6815c6c3d8
Md Bulbul Hasan <bulbulhasan095@gmail.com>
2024-10-16 19:28:50 +0600

    Initial commit

---
+++ my_portfolio/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <p class="text-[red] text-center">Hello My Friend</p>
+</template>
+
+<style scoped>
+
+</style>
